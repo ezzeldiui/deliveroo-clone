f83931126246c02491bfe1666bbb42195fee947c
ezzeldinui <ezzeldin.ui@gmail.com>
2024-12-10 23:20:24 +0400

    initial files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    viteReact(), 
-    react()],
+  plugins: [react()],
 })
